9badad4eb6c62d99b6b4e8ed6dda09e4ee23414c
nieves-mon <cunanan.nm@gmail.com>
2022-10-23 01:30:39 +0800

    Add due_date column in Task model

--- db/migrate/20221022172913_create_tasks.rb
+++ db/migrate/20221022172913_create_tasks.rb
@@ -2,7 +2,8 @@ class CreateTasks < ActiveRecord::Migration[6.1]
   def change
     create_table :tasks do |t|
       t.string :title
-      t.text :description
+      t.text :notes
+      t.datetime :due_date
       t.references :category, null: false, foreign_key: true
       t.references :user, null: false, foreign_key: true
 
