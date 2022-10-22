fcd7b25d1bec743fde5356bbbd3d282860182a45
nieves-mon <cunanan.nm@gmail.com>
2022-10-23 02:28:59 +0800

    Add show, new, and create actions

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,34 @@
 class CategoriesController < ApplicationController
     before_action :authenticate_user!
+    before_action :set_category, only: [:show]
 
     def index
         @categories = current_user.categories
     end
+
+    def show
+    end
+
+    def new
+        @category = Category.new
+    end
+
+    def create
+        @category = Category.new(category_params)
+
+        if @category.save
+            redirect_to @category
+        else
+            render :new
+        end
+    end
+
+    private
+        def category_params
+            params.require(:category).permit(:name, :user_id)
+        end
+
+        def set_category
+            @category = current_user.categories.find(params[:id])
+        end
 end
