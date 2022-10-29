dd1fe6a9296cbe28a53eb0cf32bbb420635d26b4
nieves-mon <cunanan.nm@gmail.com>
2022-10-29 23:34:11 +0800

    Create get_status method

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,2 +1,13 @@
 module CategoriesHelper
+    def get_status(category)
+      tasks = category.tasks
+
+      if tasks.where(due_date: ..Date.current, completed: false || nil).any?
+        {class: 'overdue', text: 'MAYDAY!!!'}
+      elsif tasks.where(completed: false || nil).any?
+        {class: 'pending', text: 'Still got some work to do'}
+      else
+        {class: 'finished', text: "You're all good here :D"}
+      end
+    end
 end
