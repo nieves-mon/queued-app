2eebe795daa665ee34ea15477c2678136950fc19
nieves-mon <cunanan.nm@gmail.com>
2022-11-02 02:29:18 +0800

    Add tests for no due_date and title

--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -1,7 +1,15 @@
 require "test_helper"
 
 class TaskTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save task without title" do
+    task = Task.new(title: nil, notes: 'notes', due_date: '2022-11-10', completed: false)
+    result = task.save
+    assert_not result, "Task saved without title"
+  end
+
+  test "should not save task without due_date" do
+    task = Task.new(title: 'title', notes: 'notes', due_date: nil, completed: false)
+    result = task.save
+    assert_not result, "Task saved without due_date"
+  end
 end
