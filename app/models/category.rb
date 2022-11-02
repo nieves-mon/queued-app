a5150cb72807ce05c232feeb9e6e6e97a66a2bc1
nieves-mon <cunanan.nm@gmail.com>
2022-11-02 21:50:09 +0800

    Fix bugs with validations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,6 @@
 class Category < ApplicationRecord
   belongs_to :user
   has_many :tasks, dependent: :destroy
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true
+  validates :name, uniqueness: {scope: :user_id}
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :categories
-  has_many :tasks
+  has_many :categories, dependent: :destroy
+  has_many :tasks, dependent: :destroy
 end
