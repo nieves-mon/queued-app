fd1a58ec95159a2ef701091d922a313d64111c8f
nieves-mon <cunanan.nm@gmail.com>
2022-11-03 14:45:01 +0800

    Add two addt'l tests

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -3,6 +3,8 @@ require "test_helper"
 class CategoryTest < ActiveSupport::TestCase
   setup do
     @user2 = users(:user2)
+    @category2 = categories(:two)
+    @category1 = categories(:one)
   end
 
   test "should save with all attributes" do
@@ -27,5 +29,17 @@ class CategoryTest < ActiveSupport::TestCase
     category = Category.new(name: nil, user: nil)
     result = category.save
     assert_not result, "Category saved without all attributes"
+  end
+
+  test "should not save duplicate category" do
+    category = Category.new(name: @category2.name, user: @user2)
+    result = category.save
+    assert_not result, "Duplicate category was saved"
+  end
+
+  test "should save category that is the same name owned by a different user" do
+    category = Category.new(name: @category1.name, user: @user2)
+    result = category.save
+    assert result, "Category with same name as one owned by a different user was not saved"
   end
 end
