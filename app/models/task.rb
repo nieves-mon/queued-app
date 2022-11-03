fc9815df44f0d4effe47d06ce6bef1d5a15e65af
nieves-mon <cunanan.nm@gmail.com>
2022-11-03 16:44:02 +0800

    Add validation for min and max length

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,5 +2,6 @@ class Task < ApplicationRecord
   belongs_to :category
   belongs_to :user
 
-  validates :title, :due_date, presence: true
+  validates :title, presence: true, length: { minimum: 2, maximum: 25}
+  validates :due_date, presence: true
 end
